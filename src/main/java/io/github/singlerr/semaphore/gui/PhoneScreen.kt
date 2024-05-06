f178929c6486c25160acf1f3af697ec02c3e0bec
singlerr <singlerr@naver.com>
2024-05-06 23:46:18 +0900

    init

--- src/main/java/io/github/singlerr/semaphore/gui/PhoneScreen.kt
+++ src/main/java/io/github/singlerr/semaphore/gui/PhoneScreen.kt
@@ -2,22 +2,29 @@ package io.github.singlerr.semaphore.gui
 
 import gg.essential.elementa.ElementaVersion
 import gg.essential.elementa.WindowScreen
-import gg.essential.elementa.components.ScrollComponent
-import gg.essential.elementa.components.UIBlock
-import gg.essential.elementa.components.UIImage
-import gg.essential.elementa.components.Window
+import gg.essential.elementa.components.*
 import gg.essential.elementa.constraints.*
 import gg.essential.elementa.dsl.*
+import gg.essential.elementa.utils.ResourceCache
 import io.github.singlerr.semaphore.Semaphore
 import io.github.singlerr.semaphore.utils.ResourceLocationBuilder
 import net.minecraft.client.Minecraft
+import net.minecraft.client.gui.ScaledResolution
 import net.minecraft.util.ResourceLocation
 import java.awt.Color
+import java.awt.image.BufferedImage
 import java.util.concurrent.CompletableFuture
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.ConcurrentMap
 import javax.imageio.ImageIO
 import javax.swing.WindowConstants
 
 class PhoneScreen : WindowScreen(ElementaVersion.V5) {
+
+    companion object{
+        private val cache:ConcurrentMap<ResourceLocation,BufferedImage> = ConcurrentHashMap()
+    }
+
     private val phoneFrame: ResourceLocation = ResourceLocationBuilder.builder()
         .namespace(Semaphore.MOD_ID)
         .append("textures")
@@ -25,28 +32,47 @@ class PhoneScreen : WindowScreen(ElementaVersion.V5) {
         .append("phone_frame.png")
         .build()
 
+
+
     init{
 
-
-        val frame = UIImage(CompletableFuture.supplyAsync {
-            ImageIO.read(Minecraft.getMinecraft().resourceManager.getResource(phoneFrame).inputStream)
+        val frame = UIImage(if(cache.containsKey(phoneFrame)){ CompletableFuture.completedFuture(cache[phoneFrame]) } else { CompletableFuture.supplyAsync {
+            val image = ImageIO.read(Minecraft.getMinecraft().resourceManager.getResource(phoneFrame).inputStream)
+            cache[phoneFrame] = image
+            return@supplyAsync image
+        }
         }).constrain {
-            x = 5.pixels
-            y = 5.pixels
-
-            width = RelativeWindowConstraint(0.5f)
-            height = RelativeWindowConstraint(0.5f)
+            x = 0.pixels()
+            y = 5.percent()
+            width = 40.percent()
+            height = 80.percent()
         } childOf window
 
-        val inner = UIBlock(Color.RED).constrain {
-            x = 5.pixels
-            y = 5.pixels
+        val scroll = ScrollComponent(innerPadding = 2f, scrollDirection = ScrollComponent.Direction.Vertical).constrain {
+            x = 100.percent() boundTo frame
+            y = 10.percent() boundTo frame
 
-            width = RelativeWindowConstraint(0.3f)
-            height = 10.pixels
-        } childOf window
+            width = 90.percent() boundTo frame
+            height = 100.percent() boundTo frame
+        } childOf frame
+
+        repeat(5){
+            UIBlock(Color.RED).constrain {
+                x = 5.pixels()
+                y = 5.pixels()
+                width = 100.percent() boundTo scroll
+                height = 16.pixels() boundTo scroll
+            } childOf scroll
+        }
+
 
     }
 
+    override fun drawDefaultBackground() {
 
+    }
+
+    override fun drawBackground(tint: Int) {
+
+    }
 }

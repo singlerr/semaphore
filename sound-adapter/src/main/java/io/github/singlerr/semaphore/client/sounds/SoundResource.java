77fbadc283915ea738ac79d4d525c86f4e8f5935
singlerr <singlerr@naver.com>
2024-09-18 13:14:19 +0900

    Bump to 1.4.0-SNAPSHOT

--- sound-adapter/src/main/java/io/github/singlerr/semaphore/client/sounds/SoundResource.java
+++ sound-adapter/src/main/java/io/github/singlerr/semaphore/client/sounds/SoundResource.java
@@ -2,15 +2,15 @@
 package io.github.singlerr.semaphore.client.sounds;
 
 public enum SoundResource {
-    CALL_ACCEPT("semaphore:sound_call_accept", 1),
-    CALL_REJECT("semaphore:sound_call_reject", 1),
-    CALL_CLOSE("semaphore:sound_call_close", 1),
-    BELL("semaphore:sound_bell", 1),
-    REQUESTING_CALL("semaphore:sound_requesting_call", 1),
-    RECEIVING_CALL("semaphore:sound_receiving_call", 1),
-    INTERACTION("semaphore:sound_interaction", 5),
-    VIBRATION("semaphore:sound_vibration", 5),
-    TARGET_UNAVAILABLE("semaphore:sound_target_unavailable", 1);
+    CALL_ACCEPT("semaphore:sound_call_accept", 0),
+    CALL_REJECT("semaphore:sound_call_reject", 0),
+    CALL_CLOSE("semaphore:sound_call_close", 0),
+    BELL("semaphore:sound_bell", 0),
+    REQUESTING_CALL("semaphore:sound_requesting_call", 0),
+    RECEIVING_CALL("semaphore:sound_receiving_call", 0),
+    INTERACTION("semaphore:sound_interaction", 1),
+    VIBRATION("semaphore:sound_vibration", 0),
+    TARGET_UNAVAILABLE("semaphore:sound_target_unavailable", 0);
 
     private String name;
     private int repeatDelay;

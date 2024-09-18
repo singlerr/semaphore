33e7d5db10615c6c9d3577f8d15d995cc1b24398
singlerr <singlerr@naver.com>
2024-09-18 12:31:20 +0900

    Added repeat delay to SoundResource so that other modules can persist same delay

--- sound-adapter/src/main/java/io/github/singlerr/semaphore/client/sounds/SoundResource.java
+++ sound-adapter/src/main/java/io/github/singlerr/semaphore/client/sounds/SoundResource.java
@@ -2,23 +2,29 @@
 package io.github.singlerr.semaphore.client.sounds;
 
 public enum SoundResource {
-    CALL_ACCEPT("semaphore:sound_call_accept"),
-    CALL_REJECT("semaphore:sound_call_reject"),
-    CALL_CLOSE("semaphore:sound_call_close"),
-    BELL("semaphore:sound_bell"),
-    REQUESTING_CALL("semaphore:sound_requesting_call"),
-    RECEIVING_CALL("semaphore:sound_receiving_call"),
-    INTERACTION("semaphore:sound_interaction"),
-    VIBRATION("semaphore:sound_vibration"),
-    TARGET_UNAVAILABLE("semaphore:sound_target_unavailable");
+    CALL_ACCEPT("semaphore:sound_call_accept", 1),
+    CALL_REJECT("semaphore:sound_call_reject", 1),
+    CALL_CLOSE("semaphore:sound_call_close", 1),
+    BELL("semaphore:sound_bell", 1),
+    REQUESTING_CALL("semaphore:sound_requesting_call", 1),
+    RECEIVING_CALL("semaphore:sound_receiving_call", 1),
+    INTERACTION("semaphore:sound_interaction", 5),
+    VIBRATION("semaphore:sound_vibration", 5),
+    TARGET_UNAVAILABLE("semaphore:sound_target_unavailable", 1);
 
     private String name;
+    private int repeatDelay;
 
-    SoundResource(String name) {
+    SoundResource(String name, int repeatDelay) {
         this.name = name;
+        this.repeatDelay = repeatDelay;
     }
 
     public String getName() {
         return name;
+    }
+
+    public int getRepeatDelay() {
+        return repeatDelay;
     }
 }
